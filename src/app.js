65b0dc6e4b4d9ee2b4df38414f187791c1526573
DANNIMENNA89 <146239929+DANNIMENNA89@users.noreply.github.com>
2023-11-08 19:18:08 +0000

    Incluir boimagenes

--- src/app.js
+++ src/app.js
@@ -31,4 +31,18 @@ window.onload = function() {
     " " +
     when[whenRandom];
   document.getElementById("excuse").innerHTML = frase;
+
+  if (whoRandom === 0) {
+    document.getElementById("img").src =
+      "https://img.freepik.com/vector-gratis/perro-lindo-alegre-sobre-fondo-blanco_1308-132745.jpg?size=626&ext=jpg&ga=GA1.1.1880011253.1699401600&semt=ais";
+  } else if (whoRandom === 1) {
+    document.getElementById("img").src =
+      "https://previews.123rf.com/images/yupiramos/yupiramos1709/yupiramos170920392/86318431-dise%C3%B1o-gr%C3%A1fico-de-feliz-abuela-dibujos-animados-vector-ilustraci%C3%B3n.jpg";
+  } else if (whoRandom === 2) {
+    document.getElementById("img").src =
+      "https://static.vecteezy.com/system/resources/thumbnails/005/561/633/small/turtle-cartoon-clipart-illustration-free-vector.jpg";
+  } else {
+    document.getElementById("img").src =
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBK84hnjgEPiN0c0Co2J21YAuDeKQCJ83VuNzfgbsl3chBAfKRjywS1A3F9nqowj2tuRk&usqp=CAU";
+  }
 };
